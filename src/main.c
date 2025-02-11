8ace7e3e5abc9106a0d22210505e009655520dc1
Rich <applerich0306@gmail.com>
2025-02-11 15:11:30 +0200

    commented out the ft_getpid() so other code can work normally

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: chlee2 <chlee2@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 17:56:06 by chlee2            #+#    #+#             */
-/*   Updated: 2025/02/11 12:07:06 by chlee2           ###   ########.fr       */
+/*   Updated: 2025/02/11 15:09:04 by chlee2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,42 +30,42 @@ void shell_init(char **envp, t_shell *shell)
 	shell->ambiguous_flag = 0;
 }
 
-static int ft_getpid(void)
-{
-	int fork_res;
-	int pid_pipe[2];
-	int wstatus;
-	int exit_status = 1;
-	pipe(pid_pipe);
-	fork_res = fork();
-	if (fork_res == -1)
-	{
-		close(pid_pipe[0]);
-		close(pid_pipe[1]);
-		return 1;
-	}
-	else if (fork_res)
-	{
-		write(pid_pipe[1], &fork_res, sizeof(fork_res));
-		close(pid_pipe[0]);
-		close(pid_pipe[1]);
-		wait(&wstatus);
-		if (WIFEXITED(wstatus))
-				exit_status = WEXITSTATUS(wstatus);
-			else if (WIFSIGNALED(wstatus))
-				exit_status = 128 + WTERMSIG(wstatus);
-		exit(exit_status);
-	}
-	read(pid_pipe[0], &fork_res, sizeof(fork_res));
-	close(pid_pipe[0]);
-	close(pid_pipe[1]);
-	return (fork_res);
-}
+// static int ft_getpid(void)
+// {
+// 	int fork_res;
+// 	int pid_pipe[2];
+// 	int wstatus;
+// 	int exit_status = 1;
+// 	pipe(pid_pipe);
+// 	fork_res = fork();
+// 	if (fork_res == -1)
+// 	{
+// 		close(pid_pipe[0]);
+// 		close(pid_pipe[1]);
+// 		return 1;
+// 	}
+// 	else if (fork_res)
+// 	{
+// 		write(pid_pipe[1], &fork_res, sizeof(fork_res));
+// 		close(pid_pipe[0]);
+// 		close(pid_pipe[1]);
+// 		wait(&wstatus);
+// 		if (WIFEXITED(wstatus))
+// 				exit_status = WEXITSTATUS(wstatus);
+// 			else if (WIFSIGNALED(wstatus))
+// 				exit_status = 128 + WTERMSIG(wstatus);
+// 		exit(exit_status);
+// 	}
+// 	read(pid_pipe[0], &fork_res, sizeof(fork_res));
+// 	close(pid_pipe[0]);
+// 	close(pid_pipe[1]);
+// 	return (fork_res);
+// }
 
 int	main(int ac, char **av, char **envp)
 {
 	t_shell	shell;
-	shell.shell_id = ft_getpid();
+	// shell.shell_id = ft_getpid();
 
 	(void)av;
 	if (ac != 1)
