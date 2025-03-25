d89450c7d7206b49e4dad1f741d0671b3e0bb084
Mikhail Butuzov <mbutuzov@c1r3p8.hive.fi>
2025-03-25 22:27:07 +0200

    heredoc.c mostly normed, one extra tester error appeared

--- src/pipex/heredoc.c
+++ src/pipex/heredoc.c
@@ -6,11 +6,12 @@
 /*   By: mbutuzov <mbutuzov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 17:03:34 by mbutuzov          #+#    #+#             */
-/*   Updated: 2025/03/24 19:58:04 by mbutuzov         ###   ########.fr       */
+/*   Updated: 2025/03/25 22:26:22 by mbutuzov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
 void	close_fd_safe(int *fd)
 {
 	if (*fd != -1)
@@ -19,32 +20,18 @@ void	close_fd_safe(int *fd)
 		*fd = -1;
 	}
 }
+
 void	close_pipe_safe(int fds[2])
 {
 	close_fd_safe(fds);
 	close_fd_safe(fds + 1);
 }
-/*
 
-int is_eof_with_nl(char *line, char *eof)
+int	check_heredoc(t_cmd cmd)
 {
-	size_t eof_length;
-
-	eof_length = ft_strlen(eof);
-	if (ft_strncmp(eof, line, eof_length))
-		return (0);
-	line += eof_length;
-	if (*line == '\n' && *(line + 1) == 0)
-		return (1);
-	return (0);
-}
-
-*/
-int check_heredoc(t_cmd cmd)
-{
-	size_t infile_count;
-	size_t outfile_count;
-	size_t count;
+	size_t	infile_count;
+	size_t	outfile_count;
+	size_t	count;
 
 	count = 0;
 	if (cmd.infiles)
@@ -66,126 +53,110 @@ int check_heredoc(t_cmd cmd)
 	return (0);
 }
 
-int get_cmd_heredoc(t_cmd cmd)
+int	try_get_heredoc(size_t count, t_cmd cmd)
 {
-	int fd;
-	size_t infile_count;
-	size_t outfile_count;
-	size_t count;
-	char *heredoc_eof;
+	char	*heredoc_eof;
+	int		fd;
 
+	heredoc_eof = get_redir_str(count, cmd);
+	fd = get_here_doc_fd(heredoc_eof, (t_shell *)cmd.shell);
+	if (fd == -1)
+		return (-1);
+	return (fd);
+}
+
+int	get_cmd_heredoc(t_cmd cmd)
+{
+	int		fd;
+	size_t	count;
+	size_t	file_count;
+
+	file_count = count_files(&cmd);
 	fd = -1;
 	count = 0;
-	if (cmd.infiles)
-		infile_count = (size_t)count_split(cmd.infiles);
-	else
-		infile_count = 0;
-	if (cmd.outfiles)
-		outfile_count = (size_t)count_split(cmd.outfiles);
-	else
-		outfile_count = 0;
-	while (count < infile_count + outfile_count)
+	while (count < file_count)
 	{
 		if (cmd.redirect_type[count] == HERE_DOC)
 		{
 			if (fd != -1)
-			{
 				close(fd);
-				heredoc_eof = get_redir_str(count, cmd);
-// TODO: check shell cleanup in child process
-				fd = get_here_doc_fd(heredoc_eof, (t_shell *)cmd.shell);
-				if (fd == -1)
-					return (-1);
-			}
-			else
-			{
-				heredoc_eof = get_redir_str(count, cmd);
-				fd = get_here_doc_fd(heredoc_eof, (t_shell *)cmd.shell);
-				if (fd == -1)
-					return (-1);
-			}
+			fd = try_get_heredoc(count, cmd);
+			if (fd == -1)
+				return (-1);
 		}
 		count++;
 	}
 	return (fd);
 }
 
-void print_line_ascii(char *line)
+void	heredoc_readline_loop(char *eof, int fds[2], int *proper_exit)
 {
-	if (!line)
-		return ;
-	while(*line)
+	char	*line;
+
+	line = 0;
+	line = readline("> ");
+	while (line)
 	{
-		ft_putnbr_fd(*line, 2);
-		write(2, "\n", 1);
-		line++;
+		if (!ft_strcmp(line, eof))
+		{
+			*proper_exit = 1;
+			free(line);
+			break ;
+		}
+		ft_putendl_fd(line, fds[1]);
+		free(line);
+		line = readline("> ");
 	}
 }
 
-int get_here_doc_fd(char *eof, t_shell *shell)
+char	*get_heredoc_eof_string(char *delimiter)
 {
-	//int fork_res;
-	int fds[2];
-	int fork_res;
-	char *line;
-//	int dup_res;
-	int proper_exit;
+	char	*strings[6];
+
+	strings[0] = SHELL_NAME;
+	strings[1] = ": warning: here-document at some line ";
+	strings[2] = "delimited by end-of-file (wanted '";
+	strings[3] = delimiter;
+	strings[4] = "')\n";
+	strings[5] = 0;
+	return (ft_split_join(strings, 0));
+}
+
+void	in_heredoc_child(char *eof, t_shell *shell, int fds[2])
+{
+	int		proper_exit;
+	char	*line;
 
 	line = 0;
 	proper_exit = 0;
-//	char *fineof;
-	if (pipe(fds) == -1)
-		return (-1);
-//	before_child_process_signal();
-	before_heredoc_process_signal();
-	fork_res = fork();
-	if (!fork_res)
+	set_heredoc_signal();
+	heredoc_readline_loop(eof, fds, &proper_exit);
+	close_pipe_safe(fds);
+	if (g_sig == 1)
 	{
-		set_heredoc_signal();
-		line = readline("> ");
-		while (line)
-		{
-			if (!ft_strcmp(line, eof))
-			{
-				proper_exit = 1;
-				free(line);
-				break ;
-			}
-			ft_putendl_fd(line, fds[1]);
-			free(line);
-			line = readline("> ");
-		}
-		close_pipe_safe(fds);
-		if (g_sig == 1)
-		{
-			ft_free_all(shell);
-			exit(130);
-		}
-		if (!proper_exit)
-		{
-//			bash: warning: here-document at some line delimited by end-of-file (wanted `wow')
-//TODO: adjust to be correct error message
-//			ft_putstr_fd("\nheredoc EOF stoped\n", 2);
-			if (!dup2(2, 1))
-				printf("%s: warning: here-document at some line delimited by end-of-file (wanted '%s')\n", SHELL_NAME, eof);
-		}
 		ft_free_all(shell);
-		exit(0);
+		exit(130);
 	}
-	close(fds[1]);
-	if (fork_res == -1)
+	if (!proper_exit)
 	{
-		close(fds[0]);
-		return (-1);
+		line = get_heredoc_eof_string(eof);
+		if (line)
+		{
+			ft_putstr_fd(line, 2);
+			free(line);
+		}
 	}
-/*
-	if (waitpid(fork_res, 0, 0) == fork_res)
-		return (fds[0]);
-*/
-	int wait_status;
-	int wait_asd  = wait(&wait_status);
-//	if (wait(&wait_status) == fork_res)
-	if (wait_asd == fork_res)
+	ft_free_all(shell);
+	exit(0);
+}
+
+int	wait_heredoc(int fork_res, t_shell *shell, int fds[2])
+{
+	int	wait_status;
+	int	wait_return;
+
+	wait_return = wait(&wait_status);
+	if (wait_return == fork_res)
 	{
 		set_minishell_signal();
 		if (WIFEXITED(wait_status))
@@ -204,22 +175,25 @@ int get_here_doc_fd(char *eof, t_shell *shell)
 	close(fds[0]);
 	return (-1);
 }
-/*
-int main()
+
+int	get_here_doc_fd(char *eof, t_shell *shell)
 {
-	int somefd = get_here_doc_fd("some");
-	if (somefd == -1)
-		return (1);
-	close(somefd);
-	return (0);
-	char *line = get_next_line(somefd);
-	while (line)
+	int		fds[2];
+	int		fork_res;
+	int		proper_exit;
+
+	proper_exit = 0;
+	if (pipe(fds) == -1)
+		return (-1);
+	before_heredoc_process_signal();
+	fork_res = fork();
+	if (!fork_res)
+		in_heredoc_child(eof, shell, fds);
+	close(fds[1]);
+	if (fork_res == -1)
 	{
-		ft_printf(line);
-		free(line);
-		line = get_next_line(somefd);
+		close(fds[0]);
+		return (-1);
 	}
-	close(somefd);
-	return (0);
+	return (wait_heredoc(fork_res, shell, fds));
 }
-*/
