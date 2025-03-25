e7bb78d7fbeef2ec89535596ad0f0dcc00758d16
Mikhail Butuzov <mbutuzov@c1r3p8.hive.fi>
2025-03-25 22:39:29 +0200

    execute c mostly normed

--- src/execute.c
+++ src/execute.c
@@ -6,13 +6,13 @@
 /*   By: chlee2 <chlee2@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 22:28:12 by mbutuzov          #+#    #+#             */
-/*   Updated: 2025/03/25 15:39:44 by mbutuzov         ###   ########.fr       */
+/*   Updated: 2025/03/25 22:38:51 by mbutuzov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-static int preserve_fds(t_shell *shell)
+static int	preserve_fds(t_shell *shell)
 {
 	if (shell->stdin_fd == -1)
 		shell->stdin_fd = dup(STDIN_FILENO);
@@ -27,7 +27,7 @@ static int preserve_fds(t_shell *shell)
 	return (0);
 }
 
-static int restore_fds(t_shell *shell)
+static int	restore_fds(t_shell *shell)
 {
 	if (dup2(shell->stdin_fd, STDIN_FILENO) == -1)
 		return (-1);
@@ -36,7 +36,7 @@ static int restore_fds(t_shell *shell)
 	return (0);
 }
 
-void preserve_fds_and_error_exit(t_shell *shell)
+void	preserve_fds_and_error_exit(t_shell *shell)
 {
 	int	redirection_result;
 
@@ -49,7 +49,7 @@ void preserve_fds_and_error_exit(t_shell *shell)
 	}
 }
 
-void restore_fds_and_error_exit(t_shell *shell)
+void	restore_fds_and_error_exit(t_shell *shell)
 {
 	int	redirection_result;
 
@@ -62,58 +62,54 @@ void restore_fds_and_error_exit(t_shell *shell)
 	}
 }
 
-// builtin error handing 
-void execute(t_shell *shell)
+void	cleanup_parent_on_redirection_fail(t_shell *shell)
 {
-	// printf("shel code: %d\n", shell->exit_code);
+	if (shell->cmds && shell->cmds->heredoc_fd != -1)
+	{
+		close(shell->cmds->heredoc_fd);
+		shell->cmds->heredoc_fd = -1;
+	}
+	clear_cmds(shell);
+	shell->exit_code = 1;
+	restore_fds_and_error_exit(shell);
+}
 
-	int	redirection_result;
-	int	exec_result;
-	
+void	execute_cmd_parent(t_shell *shell)
+{
+	shell->cmds->shell = shell;
+	preserve_fds_and_error_exit(shell);
+	if (!resolve_heredoc_cmds(shell->cmds, 1))
+	{
+		if (shell->err_code)
+		{
+			shell->err_code = 0;
+			restore_fds_and_error_exit(shell);
+			return ;
+		}
+		else
+		{
+			print_error_message(error_init(HEREDOC_FAIL, 0, 0));
+			ft_free_all(shell);
+			exit(1);
+		}
+	}
+	if (process_file_redirections(shell->cmds))
+	{
+		cleanup_parent_on_redirection_fail(shell);
+		return ;
+	}
+	shell->exit_code = handle_builtin(*(shell->cmds));
+	restore_fds_and_error_exit(shell);
+}
+
+void	execute(t_shell *shell)
+{
 	if (!shell->cmds)
 	{
 		return ;
 	}
 	if (shell->cmds->next || !get_builtin_type(*(shell->cmds)))
-	{
-		exec_result = pipex_launch(shell->cmds, shell->envp, shell);
-	}
+		shell->exit_code = pipex_launch(shell->cmds, shell->envp, shell);
 	else
-	{
-		shell->cmds->shell = shell;
-		preserve_fds_and_error_exit(shell);
-		if (!resolve_heredoc_cmds(shell->cmds, 1))
-		{
-			if (shell->err_code)
-			{
-				shell->err_code = 0;
-				restore_fds_and_error_exit(shell);
-				return ;
-			}
-			else
-			{
-				print_error_message(error_init(HEREDOC_FAIL, 0, 0));
-				ft_free_all(shell);
-				exit(1);
-			}
-		}
-		redirection_result = process_file_redirections(shell->cmds);
-		if (redirection_result)
-		{
-			if (shell->cmds && shell->cmds->heredoc_fd != -1)
-			{
-				close(shell->cmds->heredoc_fd);
-				shell->cmds->heredoc_fd = -1;
-			}
-			clear_cmds(shell);
-			shell->exit_code = 1;
-			restore_fds_and_error_exit(shell);
-			return ;
-		}
-//TODO: pass shell as a parameter, return to exit status
-		exec_result = handle_builtin(*(shell->cmds));
-		shell->exit_code = exec_result;
-		restore_fds_and_error_exit(shell);
-	}
-	shell->exit_code = exec_result;
+		execute_cmd_parent(shell);
 }
